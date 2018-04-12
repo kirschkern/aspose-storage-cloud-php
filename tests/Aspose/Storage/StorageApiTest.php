ec521962631087329a999b6b4004e238f6113c90
madalina.ciocan <madalina.ciocan@aspose.com>
2018-04-12 12:34:10 +0300

    Some fixes

--- tests/Aspose/Storage/StorageApiTest.php
+++ tests/Aspose/Storage/StorageApiTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * --------------------------------------------------------------------------------------------------------------------
-* <copyright company="Aspose Pty Ltd" file="Aspose\Storage\ApiTest.php">
+* <copyright company="Aspose Pty Ltd" file="Aspose\Storage\StorageApiTest.php">
 *   Copyright (c) 2018 Aspose.Storage for Cloud
 * </copyright>
 * <summary>
